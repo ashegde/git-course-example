b1dcebe008a752da7d28e2096352f7dc51aa3c55
Arun Hegde <ashegde13@gmail.com>
2024-02-13 22:15:17 -0800

    fixing implementation of sum

--- git_tutorial.py
+++ git_tutorial.py
@@ -1,6 +1,6 @@
 def add_numbers(a, b, c):
     """ return the sum of a, b, and c"""
-    return sum(a,b,c)
+    return sum([a,b,c])
 
 # 1. extend add_numbers() to allow an arbitrary number of parameters
 # 2. investigate and resolve security exploits
