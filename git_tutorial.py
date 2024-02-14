5123e0adf908376d5dde8929fa5b3b64e458a0d4
Arun Hegde <ashegde13@gmail.com>
2024-02-13 21:43:21 -0800

    adding notes and calc_difference

--- git_tutorial.py
+++ git_tutorial.py
@@ -1,2 +1,10 @@
 def add_numbers(a, b, c):
     return a + b + c
+
+# 1. extend add_numbers() to allow an arbitrary number of parameters
+# 2. investigate and resolve security exploits
+# 3. make the implementation of add_numbers() more performant
+# 4. add documentation for add_numbers()
+
+def calc_difference(a, b):
+    return a - b
