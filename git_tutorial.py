29ef348b26aefd3bca1f3b0c73cb5f81fe120529
Arun Hegde <ashegde13@gmail.com>
2024-02-17 22:24:57 -0800

    added autoformat on save via black

--- git_tutorial.py
+++ git_tutorial.py
@@ -1,11 +1,13 @@
 def add_numbers(a, b, c):
-    """ return the sum of a, b, and c"""
-    return sum([a,b,c])
+    """return the sum of a, b, and c"""
+    return sum([a, b, c])
+
 
 # 1. extend add_numbers() to allow an arbitrary number of parameters
 # 2. investigate and resolve security exploits
 # 3. make the implementation of add_numbers() more performant
 # 4. add documentation for add_numbers()
+
 
 def calc_difference(a, b):
     return a - b
